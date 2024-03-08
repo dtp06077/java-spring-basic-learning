98b4ae9ef9dd6e1afecdf977d11267c57c17f7de
dtp06077 <dtp06077@gmail.com>
2024-03-08 23:21:31 +0900

    Test: @ComponentScan의 필터 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,7 +8,9 @@ import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @ComponentScan(
-    excludeFilters = @Filter(type= FilterType.ANNOTATION, classes=
+//        탐색할 패키지의 시작 위치 지정, 아래 코드는 member패키지만 스캔
+//        basePackages="hello.core.Member",
+        excludeFilters = @Filter(type= FilterType.ANNOTATION, classes=
             Configuration.class))
 public class AutoAppConfig {
 
--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,12 +2,22 @@ package hello.core.scan;
 
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
+import hello.core.scan.filter.BeanA;
+import hello.core.scan.filter.BeanB;
+import hello.core.scan.filter.MyExcludeComponent;
+import hello.core.scan.filter.MyIncludeComponent;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.context.annotation.ComponentScan.Filter;
 
 public class AutoAppConfigTest {
 
@@ -17,5 +27,33 @@ public class AutoAppConfigTest {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService=ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+    @Test
+    @DisplayName("컴포넌트 스캔 필터 테스트")
+    void filterScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean(BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                ()->ac.getBean(BeanB.class));
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = {
+                    @Filter(type = FilterType.ANNOTATION, classes =
+            MyIncludeComponent.class),
+            },
+            excludeFilters = {
+                    @Filter(type = FilterType.ANNOTATION, classes =
+            MyExcludeComponent.class),
+                    //BeanA도 제외하고 싶을 때 추가
+                    //@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BeanA.class)
+            }
+    )
+    static class ComponentFilterAppConfig {
     }
 }
