41aa742ab8e02c99a598d1aab9384bf40ca1d824
dtp06077 <dtp06077@gmail.com>
2024-03-14 20:18:11 +0900

    Test: 프로토타입 스코프 테스트

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,42 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    @DisplayName("프로토타입 스코프 빈 테스트")
+    public void prototypeBeanTest(){
+        ConfigurableApplicationContext ac=new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1=ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2=ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        ac.close();
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void close(){
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
